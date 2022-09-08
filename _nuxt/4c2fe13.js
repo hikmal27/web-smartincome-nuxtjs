54711def8498880aa46b57a2a2d29e86c6cb4bdf
Hikmal Maulana Ramadhan <hikmal.ramadhan@smartfinance.co.id>
2022-09-08 14:38:34 +0700

    First commit

---
+++ _nuxt/4c2fe13.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[30],{392:function(n,e,t){"use strict";t.r(e);var r={setup:function(){return{}}},c=t(15),component=Object(c.a)(r,(function(){var n=this,e=n.$createElement;return(n._self._c||e)("div",[n._v("\n    Login\n")])}),[],!1,null,null,null);e.default=component.exports}}]);
